7975e59d22342ad4f62667c386a2d6ea7e45f058
Sufiyan Mansuri <mansurisufiyan141@gmail.com>
2025-12-24 15:44:57 +0530

    Added course model

--- KnowledgeBase/courses/models.py
+++ KnowledgeBase/courses/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from users import models as userModels
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
 class Category(models.Model):
@@ -12,3 +16,23 @@ class Category(models.Model):
     def __str__(self):
         return self.name
     
+class Course(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published')
+    )
+
+    title = models.CharField(max_length=256)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='courses')
+    thumbnail = models.ImageField(upload_to='course_thumbnails/')
+    instructor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='courses')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='draft')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Courses'
+
+    def __str__(self):
+        return f"{self.title} - {self.instructor.username}"
