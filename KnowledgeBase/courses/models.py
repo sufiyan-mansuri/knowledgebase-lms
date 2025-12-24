add758bfeb15eb5f5d3dc63546304c86274c0153
Sufiyan Mansuri <mansurisufiyan141@gmail.com>
2025-12-24 15:11:36 +0530

    Added category model

--- KnowledgeBase/courses/models.py
+++ KnowledgeBase/courses/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=256, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+    
